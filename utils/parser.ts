e2f15025a6146ff548750ff7c3c3cbbfffada0ce
Ashmin Jayson <ashminjayson10@gmail.com>
2023-10-28 19:41:14 +0530

    feat(parser.ts): add support for generating XLSX file with dynamic sheet headers and content

--- utils/parser.ts
+++ utils/parser.ts
@@ -1,20 +1,85 @@
 import xlsx from "json-as-xlsx";
 
-let tempDataForBuild = [
+const sheetHeaders = [
+    "SerialNo",
+    "SectionName",
+    "Tag",
+    "PositiveMark",
+    "NegativeMark",
+    "Level",
+    "AnswerTime",
+    "Instruction",
+    "AnswerExplanation",
+    "Question",
+    "QuestionType",
+    "CorrectOption",
+    "Option1",
+    "Option2",
+    "Option3",
+    "Option4",
+    "Option5",
+    "Criteria1",
+    "Percentage1",
+    "Criteria2",
+    "Percentage2",
+    "Criteria3",
+    "Percentage3",
+    "Criteria4",
+    "Percentage4",
+    "Criteria5",
+    "Percentage5",
+];
+
+const randomValue = () => Math.floor(Math.random() * 100);
+
+const mockData = Array.from({ length: 5 }, (_, i) => ({
+    SerialNo: i + 1,
+    SectionName: `Section ${i + 1}`,
+    Tag: `Tag ${i + 1}`,
+    PositiveMark: randomValue(),
+    NegativeMark: randomValue(),
+    Level: randomValue(),
+    AnswerTime: randomValue(),
+    Instruction: `Instruction ${i + 1}`,
+    AnswerExplanation: `Answer Explanation ${i + 1}`,
+    Question: `Question ${i + 1}`,
+    QuestionType: `Question Type ${i + 1}`,
+    CorrectOption: `Correct Option ${i + 1}`,
+    Option1: `Option 1 ${i + 1}`,
+    Option2: `Option 2 ${i + 1}`,
+    Option3: `Option 3 ${i + 1}`,
+    Option4: `Option 4 ${i + 1}`,
+    Option5: `Option 5 ${i + 1}`,
+    Criteria1: `Criteria 1 ${i + 1}`,
+    Percentage1: randomValue(),
+    Criteria2: `Criteria 2 ${i + 1}`,
+    Percentage2: randomValue(),
+    Criteria3: `Criteria 3 ${i + 1}`,
+    Percentage3: randomValue(),
+    Criteria4: `Criteria 4 ${i + 1}`,
+    Percentage4: randomValue(),
+    Criteria5: `Criteria 5 ${i + 1}`,
+    Percentage5: randomValue(),
+}));
+
+// console.log(data);
+
+const sheetFormat = {
+    sheetName: "QuestionFormat",
+    columns: sheetHeaders.map((headerTerm) => {
+        return {
+            label: headerTerm,
+            value: (row: any) => (row.headerTerm ? row.headerTerm : ""),
+        };
+    }),
+    content: mockData,
+};
+
+const sheet = [
     {
-        sheet: "Adults",
-        columns: [
-            { label: "User", value: (row: any) => row.user },
-            { label: "Age", value: (row: any) => row.age + " years" },
-            {
-                label: "Phone",
-                value: (row: any) => (row.more ? row.more.phone || "" : ""),
-            },
-        ],
-        content: [
-            { user: "Andrea", age: 20, more: { phone: "11111111" } },
-            { user: "Luis", age: 21, more: { phone: "12345678" } },
-        ],
+        sheet: sheetFormat.sheetName,
+        columns: sheetFormat.columns,
+        content: sheetFormat.content,
     },
 ];
 
@@ -25,5 +90,5 @@ export const parseXLSX = (fileName: string) => {
         writeMode: "writeFile",
         writeOptions: {},
     };
-    xlsx(tempDataForBuild, settings);
+    xlsx(sheet, settings);
 };
